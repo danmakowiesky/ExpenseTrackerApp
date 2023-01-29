e017211a95c6b5f705e0e380481b628a95065320
Daniel Makowiesky <daniel.makowiesky@ntendencia.com.br>
2023-01-29 15:10:21 -0300

    configurando theme inicial

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,11 +2,10 @@ import { Button } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 export const AppRoutes = () => {
-    return (
-        <Routes>
-            <Route path="/dashboard" element={<Button>Dashboard </Button>} />
-            <Route path="*" element={<Navigate to="/dashboard" />} />
-
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/dashboard" element={<Button variant="contained" color="primary">Dashboard </Button>} />
+      <Route path="*" element={<Navigate to="/dashboard" />} />
+    </Routes>
+  )
 }
---
+++ frontend/src/shared/theme/Light.ts
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material";
+import { blue, green } from "@mui/material/colors";
+
+export const LightTheme = createTheme({
+  palette: {
+    primary: {
+      main: blue[700],
+      dark: blue[800],
+      light: blue[500],
+      contrastText: '#FFFFFF',
+    },
+    secondary: {
+      main: green[700],
+      dark: green[800],
+      light: green[500],
+      contrastText: '#FFFFFF',
+    },
+    background: {
+      paper: '#FFFFFF',
+      default: '#F0F2F5',
+    }
+  }
+})
