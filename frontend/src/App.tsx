0214316b85be3836fc1603f0fd52e496860b9539
Daniel Makowiesky <daniel.makowiesky@ntendencia.com.br>
2023-01-29 14:50:46 -0300

    adicionado react router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
-function App() {
+import { BrowserRouter } from "react-router-dom"
+import { AppRoutes } from "./routes"
 
+function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+export const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/dashboard" element={<p>Dashboard</p>} />
+            <Route path="*" element={<Navigate to="/dashboard" />} />
+
+        </Routes>
+    )
+}
