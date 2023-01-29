22f6ca8273621a598336f6d55f8de692b1a658c5
Daniel Makowiesky <daniel.makowiesky@ntendencia.com.br>
2023-01-29 14:58:29 -0300

    instalando mui v5

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/dashboard" element={<p>Dashboard</p>} />
+            <Route path="/dashboard" element={<Button>Dashboard </Button>} />
             <Route path="*" element={<Navigate to="/dashboard" />} />
 
         </Routes>
